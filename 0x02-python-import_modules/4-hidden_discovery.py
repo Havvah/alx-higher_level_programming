98669ea00209ba5a849d0a7fef571e41c0d8fda3
root <havavicky@gmail.com>
2023-11-02 08:06:31 -0700

    correction to hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
     import hidden_4
-    
-    names = dir(hidden_4)
-    for name in names:
+    # Print sorted name from directory
+    for name in sorted(dir(hidden_4)):
+        # Print only names that do not start with __
         if name[:2] != "__":
-            print(name)
+            print("{}".format(name))
